753b18162e7d746e0c4c4948c56855a7447f457e
Ashley Casey <ashleymcasey@gmail.com>
2016-04-09 00:18:19 -0400

    updated API results to show based off set items within each character result

--- js/api.js
+++ js/api.js
@@ -1,41 +1,60 @@
 // Whenever the page loads, the onPageLoad function will get called.
 $(function onPageLoad() {
 
-	fetch('https://cors-anywhere.herokuapp.com/http://adventuretimeapi.com/api/v1/characters/?page_size=15')
+	// fetch url page size set to 1000 after trial and error of getting all results to show based off conditionals set below
+	fetch('https://cors-anywhere.herokuapp.com/http://adventuretimeapi.com/api/v1/characters/?page_size=1000')
 		.then(function onApiResponse(response) {
 			return response.json();
 		})
-		.then(function onJsonParsed(json) {
-			console.log(json);
 
-			/*
-				This takes the properties off the object we got from the API and
-				displays them on the page.
-				*/
-				$('#name').text(json.results[0].name);
-				$('#sex').text(json.results[0].sex);
+		.then(function onJsonParsed(charLoad) {
 
-				/*
-				using map and convertine results of relatives to text, joining with a comma in between each result
-				*/
-				$('#link').text(json.results[0].relatives_many.map(function(relative) {
+				console.log(charLoad);
+
+				var characterTemplateSource = $('#character-template').html();
+				var characterTemplate = Handlebars.compile(characterTemplateSource);
+
+				var characterListTemplateSource = $('#character-list-template').html();
+				var characterListTemplate = Handlebars.compile(characterListTemplateSource);
+				Handlebars.registerPartial('character', characterTemplateSource);
+
+				// extracting the relatives array to return a string followed by a comma
+				for(var i = 0; i < charLoad.results.length; i++) {
+					var character = charLoad.results[i];
+					character.relativeNames = character.relatives_many.map(function(relative) {
 					return relative.name;
-				}).join(", "));
+					}).join(", ");
+				}
 
-				/* populating image */
-				var $img = $("#character").attr('src', json.results[0].image);
-  				$('#character').empty().append($img);
-		});
+				// same thing here but with a species name
+				for(var i = 0; i < charLoad.results.length; i++) {
+					var character = charLoad.results[i];
+					character.speciesType = character.species.map(function(specie) {
+					return specie.name;
+					}).join(", ");
+				}
+
+				// set of conditions to ensure only results show if character has an entry for each of these items
+				charLoad.results = charLoad.results.filter(function resultFilterOptions(character) {
+					if (character.sex === "" || character.image === "" || character.relativeNames === "" || character.speciesType === "") {
+						return false;
+					} else {
+						return true;
+					}
+				})
+
+				var characterListHtml = characterListTemplate(charLoad);
+
+				$('#multiple-characters').html(characterListHtml);
+		})
 
 
-
-
+	// todo - make above a function to eliminate repition
 	// Whenever the search button is clicked, the onSearch function will get called.
 	$('#search').click(function onSearch() {
 
 		// This gets whatever the user has typed into the query textbox.
 		var query = $('#query').val();
-
 		/*
 		fetch(url)
 		Adding on whatever the value of the `query` variable is
@@ -45,7 +64,6 @@ $(function onPageLoad() {
 		we're saying that whenever the AJAX request to `url` finishes, call the `onApiResponse`
 		function we passed into `then`.
 		*/
-
 
 		fetch('https://cors-anywhere.herokuapp.com/http://adventuretimeapi.com/api/v1/characters/?search=' + query)
 			.then(function onApiResponse(response) {
@@ -58,32 +76,31 @@ $(function onPageLoad() {
 				return response.json();
 
 			})
-			.then(function onJsonParsed(json) {
+			.then(function onJsonParsed(charLoad) {
 
-				/*
-				This `onJsonParsed` function will get called once we've completed the API call
-				and have parsed out the JSON response. Now `json` is a plain javascript object with
-				whatever the API returned. 
-				*/
-				console.log(json);
+				console.log(charLoad);
 
-				/*
-				This takes the properties off the object we got from the API and
-				displays them on the page.
-				*/
-				$('#name').text(json.results[0].name);
-				$('#sex').text(json.results[0].sex);
+				var characterTemplateSource = $('#character-template').html();
+				var characterTemplate = Handlebars.compile(characterTemplateSource);
 
-				/*
-				using map and convertine results of relatives to text, joining with a comma in between each result
-				*/
-				$('#link').text(json.results[0].relatives_many.map(function(relative) {
+				var characterListTemplateSource = $('#character-list-template').html();
+				var characterListTemplate = Handlebars.compile(characterListTemplateSource);
+				Handlebars.registerPartial('character', characterTemplateSource);
+
+
+				for(var i = 0; i < charLoad.results.length; i++) {
+					var character = charLoad.results[i];
+					character.relativeNames = character.relatives_many.map(function(relative) {
 					return relative.name;
-				}).join(", "));
+					}).join(", ");
+				}
 
-				/* populating image */
-				var $img = $("#character").attr('src', json.results[0].image);
-  				$('#character').empty().append($img);
+				charLoad.results[0].relatives_many.map(function(relative) {
+					return relative.name;
+				}).join(", ");
+				var characterListHtml = characterListTemplate(charLoad);
+
+				$('#multiple-characters').html(characterListHtml);
 			});
 
 	});
